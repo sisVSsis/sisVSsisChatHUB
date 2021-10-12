e5a89424878fdeafd20839d0ef054eec28240d93
sehermahmud <seher.mahmud@gmail.com>
2021-10-12 22:07:07 +0600

    Added3

---
+++ build/precache-manifest.aafe9600f5fd7f4aea2824584dfec183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b79fdb208f9a84a03112dc7fe181e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e6d1cc7963911b6327c",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "3acfd974ed69684bbe8b",
+    "url": "/static/js/2.08b6636c.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.08b6636c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6d1cc7963911b6327c",
+    "url": "/static/js/main.14e5c7b7.chunk.js"
+  },
+  {
+    "revision": "8dd59dcf0788b7a964b2",
+    "url": "/static/js/runtime-main.9a83e805.js"
+  }
+]);
